38dfdbf978d444558b5efb68d2c4c949adccc40b
Nicolás Miari <nicolasmiari@gmail.com>
2022-07-12 11:41:29 +0900

    Update Component dependency version.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["RigidBody"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/nicolas-miari/Component.git", from: "0.0.0"),
+      .package(url: "https://github.com/nicolas-miari/Component.git", from: "0.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
