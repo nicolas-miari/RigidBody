44217cc927c8ee26f5a7d68facec3321fe482339
Nicolás Miari <nicolasmiari@gmail.com>
2022-08-14 13:17:53 +0900

    Update Component dependency to version 1.0.0 (codable component list).

--- Package.swift
+++ Package.swift
@@ -10,24 +10,21 @@ let package = Package(
       .macOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "RigidBody",
-            targets: ["RigidBody"]),
+      .library(
+          name: "RigidBody",
+          targets: ["RigidBody"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/nicolas-miari/Component.git", from: "0.0.2"),
+      .package(url: "https://github.com/nicolas-miari/Component.git", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "RigidBody",
-            dependencies: [
-              .product(name: "Component", package: "Component"),
-            ]),
-        .testTarget(
-            name: "RigidBodyTests",
-            dependencies: ["RigidBody"]),
+      .target(
+        name: "RigidBody",
+        dependencies: [
+          .product(name: "Component", package: "Component"),
+        ]),
+      .testTarget(
+        name: "RigidBodyTests",
+        dependencies: ["RigidBody"]),
     ]
 )
